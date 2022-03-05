692a78318a5ed5e78ee40d3c1e50bbf0c86cf48e
David Godfrey <david.godfrey99@gmail.com>
2022-03-05 00:10:26 -0500

    Initial commit

---
+++ path-algorithms/breadth-first-search/InputBox.py
@@ -0,0 +1,55 @@
+import pygame as pg
+import pdb
+
+pg.init()
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+        self.active_count = 0
+        self.RETURN_HIT = pg.USEREVENT + 1
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    pg.event.post(pg.event.Event(self.RETURN_HIT))
+                elif event.key == pg.K_BACKSPACE:
+                    self.active_count += 1
+                    self.text = self.text[:-1]
+                else:
+                    self.active_count += 1
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        if self.active_count == 0:
+          self.text = ""
+
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
